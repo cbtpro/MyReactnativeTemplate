7bcec064adee1225ec955c699ddb5e92c73c8bf2
chenbitao <cbtpro@gmail.com>
2025-08-12 18:53:44 +0800

    feat: 重构 App;增加别名

---
+++ src/screens/Demo/Demo.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StyleSheet, View } from 'react-native';
+
+function Demo() {
+
+  return (
+    <View style={styles.container}>
+      <NewAppScreen templateFileName="App.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Demo;
